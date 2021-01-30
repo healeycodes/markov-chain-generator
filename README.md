ddde665749d1dbf0401b5e5c50efbf9327cf9d6a
Andrew <healeycodes@gmail.com>
2021-01-30 04:27:15 +0000

    Add project

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # markov-chain-generator
+
+Use this library to generate random realistic text via Markov chain models.
+
+<br>
+
+See the example by running `python generate.py`.
+
+<br>
+
+Pass in a corpus, a state size, and a minimum length. Make sure the corpus uses capital letters and full-stops.
+
+```
+state_size = 2
+length = 100
+source = get_source('poe') # or some corpus as a string
+poe = build_model(source, state_size)
+text = generate_text(poe, state_size, 100)
+print(text)
+
+'''
+And really between two and three, there is no comfortable feeling. But the little spalpeen Mounseer Maiter-di-dauns that plumped his silf right down by the pond. Upon his own house, when wit and jollity reigned supreme-dampened a little, of course, expressed in regard to his conscience and himself. But although Hop-Frog, through the nose; the intestines through an avenue two miles long, and might have been a little amused. When we had great reason to congratulate ourselves upon our good fortune. Approaching the table, and in the way of indemnification inserted his left thumb in the external world, when, with many a fine thing.
+'''
+```
+
+<br>
+
+<small>
+The Edgar Allen Poe and Oscar Wilde text corpuses were taken from https://github.com/nlp-compromise/nlp-corpus
+
+and I'm unsure of their license. The code is MIT.
+</small>
---
+++ generate.py
@@ -0,0 +1,64 @@
+import os
+import json
+import random
+
+
+def get_source(dir):
+    '''
+    Look inside src/{dir}/ for text files and concatenate their contents.
+    '''
+    text = ""
+    files = [
+        # os.path.join to keep things cross-platform friendly
+        os.path.join('src', dir, name) for name in os.listdir(os.path.join('src', dir))]
+    for name in files:
+        with open(name, encoding="utf8") as f:
+            text += "\n"
+            text += f.read()
+    return text
+
+
+def build_model(source, state_size):
+    '''
+    Given some corpus and a state size, build a Markov chain model (as a dictionary).
+    '''
+    source = source.split()
+    model = {}
+    for i in range(state_size, len(source)):
+        current_word = source[i]
+        previous_words = ' '.join(source[i-state_size:i])
+        if previous_words in model:
+            model[previous_words].append(current_word)
+        else:
+            model[previous_words] = [current_word]
+        i += 1
+
+    return model
+
+
+def generate_text(model, state_size, length):
+    '''
+    Consume a Markov chain model (make sure to specify the state_size used)
+    to generate text that is at least <length> size long.
+    '''
+    possible_starters = random.sample(list(model.keys()), len(model.keys()))
+    text = next(s.split(' ') for s in possible_starters if s[0].isupper())
+
+    i = state_size
+    while True:
+        key = ' '.join(text[i-state_size:i])
+        next_word = random.choice(model[key])
+        text.append(next_word)
+        i += 1
+        if i > length and text[-1][-1] == '.':
+            break
+    return ' '.join(text)
+
+
+if __name__ == "__main__":
+    state_size = 2
+    length = 100
+    source = get_source('poe')
+    poe = build_model(source, state_size)
+    text = generate_text(poe, state_size, 100)
+    print(text)
